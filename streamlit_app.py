b66579738d6d78ecb681bb00db59b113486971c3
Meerabnair6 <127286168+Meerabnair6@users.noreply.github.com>
2023-03-08 12:40:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-streamlit.title('My Mom''s New Healthy Diner')
+streamlit.title('My Mom"s New Healthy Diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🍲Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥬Kale, Spinach & Rocket smoothie')
 streamlit.text('🐔Hard-Boiled Free- Ranged Egg')
 streamlit.text('🥑🍞Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
